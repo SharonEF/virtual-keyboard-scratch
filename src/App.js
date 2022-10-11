689188c6d7b6cf39a7a8a65978b013d8320d5496
Sharon Elizabeth Freundlich <elizabeth.freundlich@gmail.com>
2022-10-11 20:29:30 +0300

    virtual keyboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './keyboard.js';
 import './App.css';
 
@@ -6,17 +6,14 @@ import './App.css';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+           My first react project
           </a>
         </header>
       </div>
